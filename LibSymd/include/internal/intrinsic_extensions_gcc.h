59f79b9df7d4007ddf5e23cd68158eb9569a09d6
Nemanja Grujic <ngrujic@gmail.com>
2021-05-08 19:36:58 +0200

    * Commented out extensions for linux

--- LibSymd/include/internal/intrinsic_extensions_gcc.h
+++ LibSymd/include/internal/intrinsic_extensions_gcc.h
@@ -1,7 +1,7 @@
 #pragma once
 #ifndef _WIN32
 
-__attribute__((always_inline)) inline
+/*__attribute__((always_inline)) inline
 __m128i _mm_loadu_si64(void const* mem_addr)
 {
     __m128i ret;
@@ -31,6 +31,6 @@ __m256i _mm256_set_m128i(__m128i hi, __m128i lo)
     __m256i ret;
     asm("vinsertf128 $1, %x1, %t2, %t0" : "=x" (ret) : "x" (hi), "x" (lo));
     return ret;
-}
+}*/
 
 #endif
